d92bc829aae7633434d9defa7b8b00c6e3434a77
Jan Holst Jensen <jan@biochemfusion.com>
2017-08-10 15:27:50 +0200

    Add dispatch_to_str() to centralize conversion of Dispatch objects. Remove usage of print() as it causes spurious evaluation failures (probable threading/timing issue).

--- src/rdkitXL_server.py
+++ src/rdkitXL_server.py
@@ -1,31 +1,41 @@
-# modified from Pippo_server.py test example from win32com
+# Modified from Pippo_server.py test example from win32com.
 import sys, os
 import pythoncom
 import win32com
 import winerror
 from win32com.server.util import wrap
+from win32com.client import Dispatch
+import win32api
+
+from py2idl import generate_idl
+
 import rdkit
 from rdkit import Chem
 from rdkit.Chem import Descriptors, AllChem
-from win32com.client import Dispatch
-from py2idl import generate_idl
+
+def dispatch_to_str(possible_dispatch):
+	if not isinstance(possible_dispatch, unicode):
+		return str(Dispatch(possible_dispatch))
+	else:
+		return str(possible_dispatch)
+
 
 class CRDKitXL:
 	#
 	# COM declarations	
 	#
-	_reg_clsid_ = '{e4d5c553-ebc8-49ca-bacf-4947ef110fc5}' #"{05AC1CCE-3F9B-4d9a-B0B5-DFE8BE45AFA8}"
-	_reg_desc_ = "RDKitXL object"
-	_reg_progid_ = "Python.RDKitXL"
-	_reg_options_ = {"Programmable":''}
-	#_reg_clsctx_ = pythoncom.CLSCTX_LOCAL_SERVER	
-	###
 	#To generate new GUID's if starting a completely new project
 	#import uuid
-	#str(uuid.uuid4())
-	
+	#str(uuid.uuid4())	
+	_reg_clsid_ = '{e4d5c553-ebc8-49ca-bacf-4947ef110fc5}'
+	_reg_desc_ = "RDKitXl object"
+	_reg_progid_ = "Python.RDKitXL"
+	_reg_options_ = {"Programmable":''}
+	# Use the following line to run the server as out-of-process service. Slower, but memory-isolated from client.
+	# _reg_clsctx_ = pythoncom.CLSCTX_LOCAL_SERVER	
+
 	### Link to typelib
-	_typelib_guid_ =  '{da5bc306-15b4-498a-9c1c-f560aa8b5c32}'#'{41059C57-975F-4B36-8FF3-C5117426647A}' #
+	_typelib_guid_ =  '{da5bc306-15b4-498a-9c1c-f560aa8b5c32}'
 	_typelib_version_ = 1, 0
 	_com_interfaces_ = ['IRDKitXL']
 
@@ -34,43 +44,47 @@ class CRDKitXL:
 		self.rdkit_version = rdkit.__version__
 #RDKITXL: prop:int
 		self.number_of_calls = 0
-		
 
-#string variables given as default must be unicode and not contain " inside the string.
+# String parameters with default values:
+#   Values must be unicode strings.
+#   Values may not contain " inside the string.
+
+# Debug output: Use win32api.OutputDebugString() and something like DbgView
+# to trace it. Do not use print() - it will cause "Bad file descriptor" failures
+# (suspect that it is a threading issue).
+
 #RDKITXL: in:smiles:str, inopt:descriptor:str, out:float
 	def rdkit_descriptor(self, smiles, descriptor=u'MolLogP'):
-		self.number_of_calls = self.number_of_calls +1
-		print type(smiles), type(descriptor)
-		if not isinstance(smiles, unicode):
-			print "converting smiles"
-			smiles = str(Dispatch(smiles))
-		if not isinstance(descriptor, unicode):
-			print "converting descriptor"
-			descriptor = str(Dispatch(descriptor))
-		myfunction = getattr(Descriptors, descriptor)
-		mol = Chem.MolFromSmiles(smiles)
-		if mol != None:
-			return myfunction(mol)
-		else:
-			return 'Error in parsing SMILES'
-			
+		try:
+			self.number_of_calls = self.number_of_calls + 1
+			# win32api.OutputDebugString(str(type(smiles)) + " " + str(type(descriptor)))
+			smiles = dispatch_to_str(smiles)
+			descriptor = dispatch_to_str(descriptor)
+
+			myfunction = getattr(Descriptors, descriptor)
+			mol = Chem.MolFromSmiles(smiles)
+			if mol != None:
+				return myfunction(mol)
+			else:
+				return 'Error in parsing SMILES'
+		except Exception, e:
+			return "ERROR: " + str(e)
+		
 #RDKITXL: in:smiles:str, out:str
 	def rdkit_SmilesToMolBlock(self, smiles):
 		self.number_of_calls = self.number_of_calls +1
-		print type(smiles)
-		if not isinstance(smiles, unicode):
-			print "converting smiles"
-			smiles = str(Dispatch(smiles))
-		mol = Chem.MolFromSmiles(smiles)
-		
+		# win32api.OutputDebugString(str(type(smiles)))
+		smiles = dispatch_to_str(smiles)
+
+		mol = Chem.MolFromSmiles(smiles)		
 		if mol != None:
 			#Add coords for depiction
 			AllChem.Compute2DCoords(mol)
 			return Chem.MolToMolBlock(mol)
 		else:
 			return 'Error in parsing SMILES'
-			
-	
+
+
 def BuildTypelib(idlfile = "RDKitXL.idl"):
 	from distutils.dep_util import newer
 	this_dir = os.path.dirname(__file__)
@@ -86,7 +100,7 @@ def BuildTypelib(idlfile = "RDKitXL.idl"):
 		# just nuke them
 		for fname in ("dlldata.c %s_i.c %s_p.c %s.h"%(basename, basename, basename)).split():
 			os.remove(os.path.join(this_dir, fname))
-	
+
 	print "Registering %s" % (tlb,)
 	tli=pythoncom.LoadTypeLib(tlb)
 	pythoncom.RegisterTypeLib(tli,tlb)
